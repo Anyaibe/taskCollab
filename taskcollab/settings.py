96736c069de1ff2c9fa78298a0e8c03ddb79de9b
Anyaibe <132780892+Anyaibe@users.noreply.github.com>
2026-05-07 13:27:21 +0100

    Update settings.py

--- taskcollab/settings.py
+++ taskcollab/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SK")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['taskCollab.onrender.com']
 
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
